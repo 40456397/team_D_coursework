af58be4a43c6093283d8403cae7e49de344f68bc
willfrancis-napier <40456397@live.napier.ac.uk>
2021-02-13 15:49:22 +0000

    Added new Code of Conduct and changes from Andreas

--- App.java
+++ App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("SQL Still to be added");
+        System.out.println("SQL queries to be added here");
     }
 }
