3b3970da5483c461555e64bda0e2be452411d707
40509180 <78037988+40509180@users.noreply.github.com>
2021-03-16 16:34:47 +0000

    Travis integration - first commit & push

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,19 +8,17 @@ public class App {
      */
     private Connection con = null;
 
-    public static void main(String[] args) {
-        App app = new App();
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
         // Connect to database
-        app.connect();
-
-//        app.displayCountries();
-
-
+        a.connect("localhost:33060");
 
         // Disconnect from database
-        app.disconnect();
+        a.disconnect();
     }
-
     private void runReport1() {
     }
 
@@ -52,35 +50,39 @@ public class App {
     /**
      * Connect to the MySQL database.
      */
-    public void connect() {
-        try {
+    public void connect(String location)
+    {
+        try
+        {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i) {
+        for (int i = 0; i < retries; ++i)
+        {
             System.out.println("Connecting to database...");
-            try {
-                // Wait a bit for db to start needed for travis but can be removed locally if db running
+            try
+            {
+                // Wait a bit for db to start
                 Thread.sleep(30000);
-
-                // Connect to database locally
-//                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=true", "root", "example");
-
-                // Connect to database inside docker
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            } catch (SQLException sqle) {
+            }
+            catch (SQLException sqle)
+            {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
-                // print
+            }
+            catch (InterruptedException ie)
+            {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
