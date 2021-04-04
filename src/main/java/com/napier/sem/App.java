95fdeadb454487f8d849a6ae0cec44d370cd5cd3
willfrancis-napier <40456397@live.napier.ac.uk>
2021-04-04 13:38:09 +0100

    Added Integration tests

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,18 +9,29 @@ public class App {
      */
     private Connection con = null;
 
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) throws SQLException {
         // Create new Application
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        if (args.length < 1)
+        {
+            a.connect("localhost:3306");
+        }
+        else
+        {
+            a.connect(args[0]);
+        }
+
+        // Get Country
+        Country co = a.getCountry("United Kingdom");
 
         // Disconnect from database
         a.disconnect();
+
     }
-    private void runReport1() {
+    private void runReport2() {
         try {
             // Create variable for user input
             String selCont = JOptionPane.showInputDialog(null, "Please enter the required Continent");
@@ -29,8 +40,8 @@ public class App {
             Statement stmt = con.createStatement();
 
             // Create string for SQL statement
-            String strSelect = "SELECT Code, Name, Continent, Region, Population, Capital from country\n" +
-                    "WHERE Continent = " +selCont+ "\n" +
+            String strSelect = "SELECT Code, Name, Continent, Region, Population, Capital from country " +
+                    "WHERE Continent = " +selCont+ ", " +
                     "ORDER BY Population desc;";
 
             // Execute SQL statement
@@ -42,7 +53,7 @@ public class App {
                 String continent = rset.getString("Continent");
                 String region = rset.getString("Region");
                 int count = rset.getInt("population");
-                String capital = rset.getString("Capital");
+                int capital = rset.getInt("Capital");
                 System.out.println(name + "\t\t" + count);
             }
         } catch (Exception e) {
@@ -51,7 +62,7 @@ public class App {
         }
     }
 
-    private void displayCountries() {
+    private void runReport1() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -60,7 +71,7 @@ public class App {
             //could break this down into two SQL statements to retrieve employee details using joins
             // then another query to get the manager
 
-            String strSelect = "select Code, Name, Continent, Region, Population, Capital from country\n" +
+            String strSelect = "select Code, Name, Continent, Region, Population, Capital from country " +
                     "order by population desc;";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
@@ -72,13 +83,41 @@ public class App {
                 String continent = rset.getString("Continent");
                 String region = rset.getString("Region");
                 int count = rset.getInt("population");
-                String capital = rset.getString("Capital");
+                int capital = rset.getInt("Capital");
                 System.out.println(name + "\t\t" + count);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get details");
         }
+    }
+
+    /**
+     * Test returning Country info
+     * @return
+     */
+    public Country getCountry(String testCode) throws SQLException {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Capital FROM country WHERE Code = '" + testCode + "';";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return country if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Country co = new Country();
+                co.code = rset.getString("Code");
+                co.name = rset.getString("Name");
+                co.continent = rset.getString("Continent");
+                co.capital = rset.getInt("Capital");
+                return co;
+            }
+            else
+                return null;
+
     }
 
     /**
